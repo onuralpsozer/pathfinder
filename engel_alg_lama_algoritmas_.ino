e05b07ec22c27904be4aa5ffd282a83151a90926
kursateren <kursaterenuzun@gmail.com>
2016-08-02 13:04:48 +0300

    Engel algılama

---
+++ engel_alg_lama_algoritmas_.ino
@@ -0,0 +1,82 @@
+int ultrasonicPin = 7; //ultrasonic sensor data read pin
+int filterSize = 1;
+
+int farBoundary=170;
+int middleBoundary = 150; //Distance Zones
+int immediateBoundary = 130;
+
+int rightMotorPin = 5; //right motor speed control pin
+int leftMotorPin = 6; //left motor speed control pin
+
+
+
+
+
+//output: returns distance in cm between maxDistance and minDistance
+int getDistance(){
+  int distance = 0;
+  while(true)
+  {
+    distance = ( pulseIn(ultrasonicPin, HIGH) / 147 ) * 2.54;
+      Serial.println(distance);
+      return distance;
+    
+  }
+}
+
+//controls motors to signal obstacles in the path
+void runPathfinder(){
+  int distance = getDistance();
+  if(immediateBoundary > distance ){
+    Serial.print("its immediate: ");
+    Serial.print(distance);
+    Serial.println();
+    analogWrite(rightMotorPin,255 );
+    analogWrite(leftMotorPin, 255);
+    delay(200);
+  }else if(middleBoundary > distance){
+    Serial.print("its middle: ");
+    Serial.print(distance);
+    Serial.println();
+    analogWrite(rightMotorPin, 180);
+    analogWrite(leftMotorPin, 180);
+    delay(200);
+  }else if(farBoundary > distance){
+    Serial.print("its far: ");
+    Serial.print(distance);
+    Serial.println();
+    analogWrite(rightMotorPin, 100);
+    analogWrite(leftMotorPin, 100);
+    delay(200);
+    
+  }
+  
+  analogWrite(rightMotorPin,0);
+  analogWrite(leftMotorPin, 0);
+}
+
+
+
+
+
+
+
+void setup() {
+  pinMode(ultrasonicPin, INPUT);
+  pinMode(rightMotorPin, OUTPUT);
+  pinMode(leftMotorPin, OUTPUT);
+  //pinMode(buzzer,OUTPUT);
+  //pinMode(buton,INPUT);
+  //pinMode(x_ekseni,INPUT);
+  //pinMode(y_ekseni,INPUT);
+  //pinMode(guc,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  //digitalWrite(guc,HIGH);
+  
+  runPathfinder();
+  
+}
+
