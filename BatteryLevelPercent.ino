3e73e5618924a2db636f98f23227d89b7e158c37
mehmetalibaysal <mehmetaliibaysal@gmail.com>
2016-08-20 15:59:34 +0300

    Update BatteryLevelPercent.ino

--- BatteryLevelPercent.ino
+++ BatteryLevelPercent.ino
@@ -1,19 +1,24 @@
 int batteryLevelPin = A1;
 
-
 void setup() {
   Serial.begin(9600);
 }
-  void loop() {
+  void loop(){
   int batteryLevelValue = 0;
   float batteryLevelVoltage = 0;
   batteryLevelValue = analogRead(batteryLevelPin);
   batteryLevelVoltage = batteryLevelValue * (5.0 / 1023.0);
   Serial.print(batteryLevelVoltage);
   Serial.println();
-  delay (500);
+  Serial.print("%");
+  if(batteryLevelVoltage < 3){
+    Serial.print("% 0");
+    Serial.println();
+  }
+  else {
   float BatteryLevelPercent = (batteryLevelVoltage - 3.0)*100.0/1.2;
   Serial.print(BatteryLevelPercent);
   Serial.println();
-  delay (500);
+  }
+  delay (1000);
 }
